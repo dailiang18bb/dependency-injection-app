193fe70cc3c3a3cfc27d95e1a656edc1632bbb43
Charles <dailiang18bb@gmail.com>
2020-07-07 16:47:52 -0400

    project initial commit

---
+++ src/main/java/charles/spring/dependencyinjectionapp/DependencyInjectionAppApplication.java
@@ -0,0 +1,13 @@
+package charles.spring.dependencyinjectionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInjectionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/charles/spring/dependencyinjectionapp/DependencyInjectionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package charles.spring.dependencyinjectionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
